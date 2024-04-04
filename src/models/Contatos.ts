918423938f1b778aabba93dc7eeca6a1c0bc6eaa
ArielChaves <139404966+ArielChaves@users.noreply.github.com>
2024-04-04 17:21:35 -0300

    Contato.ts

--- src/models/Contatos.ts
+++ src/models/Contatos.ts
@@ -1,1 +1,15 @@
-ll
+class Contato {
+  id: number
+  nome: string
+  email: string
+  telefone: string
+
+  constructor(id: number, nome: string, email: string, telefone: string) {
+    this.id = id
+    this.nome = nome
+    this.email = email
+    this.telefone = telefone
+  }
+}
+
+export default Contato
