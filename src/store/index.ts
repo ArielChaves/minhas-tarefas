4a28be54f76b05b78a0f48cace0d360c23dfd0ff
ArielChaves <139404966+ArielChaves@users.noreply.github.com>
2024-04-04 17:23:47 -0300

    index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,13 @@
-    import { configureStore } from '@reduxjs/toolkit'
-    import FiltroReducer from './reducers/Filtro'
-    import atividadesReducer from './reducers/atividades'
+import { configureStore } from '@reduxjs/toolkit'
+import contatosReducer from './reducers/contatos'
+import filtroReducer from './reducers/filtro'
 
-    const store = configureStore({
-    reducer: {
-        atividade: atividadesReducer,
-        filtro: FiltroReducer
-    }
-    })
+const store = configureStore({
+  reducer: {
+    contatos: contatosReducer,
+    filtro: filtroReducer
+  }
+})
 
-    export type RootReducer = ReturnType<typeof store.getState>
-    export default store
+export type RootReducer = ReturnType<typeof store.getState>
+export default store
